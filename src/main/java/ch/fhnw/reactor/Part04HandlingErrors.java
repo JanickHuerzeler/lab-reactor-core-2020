e51d2aa35e00c28d09982ec34402823fe45f129d
Janick <janick.huerzeler@students.fhnw.ch>
2020-12-06 20:43:50 +0100

    Working on Part 04

--- src/main/java/ch/fhnw/reactor/Part04HandlingErrors.java
+++ src/main/java/ch/fhnw/reactor/Part04HandlingErrors.java
@@ -14,69 +14,74 @@ import reactor.core.publisher.Mono;
 public class Part04HandlingErrors {
 
     /**
-     * TODO 04.01
+     * 04.01
      * <p>
-     * Map the received flux into numbers using {@link Integer#parseInt} and observe that in case of
-     * {@link NumberFormatException} the flux is terminated with error.
-     * Use the log operator to view that the flux is terminated with an error.
+     * Map the received flux into numbers using {@link Integer#parseInt} and observe
+     * that in case of {@link NumberFormatException} the flux is terminated with
+     * error. Use the log operator to view that the flux is terminated with an
+     * error.
      */
     public Flux<Integer> errorIsTerminal(Flux<String> numbers) {
-        return null;
+        return numbers.log().map(Integer::parseInt).log();
     }
 
     /**
-     * TODO 04.02
+     * 04.02
      * <p>
-     * Map the received flux into numbers using {@link Integer#parseInt} and provide a fallback of 0 in case of
-     * {@link NumberFormatException}.
-     * Use the log operator to view that the flux is terminated successfully.
+     * Map the received flux into numbers using {@link Integer#parseInt} and provide
+     * a fallback of 0 in case of {@link NumberFormatException}. Use the log
+     * operator to view that the flux is terminated successfully.
      */
     public Flux<Integer> handleErrorWithFallback(Flux<String> numbers) {
-        return null;
+        return numbers.map(Integer::parseInt).onErrorReturn(0);
     }
 
-
     /**
-     * TODO 04.03
+     * 04.03
      * <p>
-     * Map the received flux into numbers using {@link Integer#parseInt} and provide a fallback of 0 in case of
-     * {@link NumberFormatException} and continue with other items.
+     * Map the received flux into numbers using {@link Integer#parseInt} and provide
+     * a fallback of 0 in case of {@link NumberFormatException} and continue with
+     * other items.
      * <p>
      * Use the flatMap and check where you put the onErrorReturn operator
      */
     public Flux<Integer> handleErrorAndContinue(Flux<String> numbers) {
-        return null;
+        return numbers.map(str -> {
+            try {
+                return Integer.parseInt(str);
+            } catch (NumberFormatException ex) {
+                return 0;
+            }
+        });
     }
 
     /**
-     * TODO 04.04
+     * 04.04
      * <p>
-     * Map the received flux into numbers using {@link Integer#parseInt} and provide an empty Mono when
-     * {@link NumberFormatException} is occurred and continue with other items.
+     * Map the received flux into numbers using {@link Integer#parseInt} and provide
+     * an empty Mono when {@link NumberFormatException} is occurred and continue
+     * with other items.
      */
     public Flux<Integer> handleErrorWithEmptyMonoAndContinue(Flux<String> numbers) {
-        return null;
-
+        return numbers.map(Integer::parseInt).onErrorContinue(e -> e instanceof NumberFormatException, (t, o) -> {});
     }
-
 
     /**
      * TODO 04.05
      * <p>
-     * For each item call received in colors flux call the {@link #simulateRemoteCall} operation.
-     * Timeout in case the {@link #simulateRemoteCall} does not return within 400 ms, but retry twice
-     * If still no response then provide "default" as a return value
+     * For each item call received in colors flux call the
+     * {@link #simulateRemoteCall} operation. Timeout in case the
+     * {@link #simulateRemoteCall} does not return within 400 ms, but retry twice If
+     * still no response then provide "default" as a return value
      */
     public Flux<String> timeOutWithRetry(Flux<String> colors) {
-        return null;
+        //return colors.map(col -> simulateRemoteCall(col).time).timeout(Duration.ofMillis(400));
     }
 
     public Mono<String> simulateRemoteCall(String input) {
         int delay = input.length() * 100;
-        return Mono.just(input)
-                .doOnNext(s -> log.info("Received {} delaying for {} ", s, delay))
-                .map(i -> "processed " + i)
-                .delayElement(Duration.of(delay, ChronoUnit.MILLIS));
+        return Mono.just(input).doOnNext(s -> log.info("Received {} delaying for {} ", s, delay))
+                .map(i -> "processed " + i).delayElement(Duration.of(delay, ChronoUnit.MILLIS));
     }
 
 }
