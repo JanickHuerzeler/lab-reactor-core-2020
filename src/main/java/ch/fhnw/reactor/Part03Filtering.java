410fa2aa9573352b6bf9328e6b5101125cf73f4d
Janick <janick.huerzeler@students.fhnw.ch>
2020-12-06 20:09:30 +0100

    Part 03

--- src/main/java/ch/fhnw/reactor/Part03Filtering.java
+++ src/main/java/ch/fhnw/reactor/Part03Filtering.java
@@ -11,66 +11,62 @@ import reactor.core.publisher.Mono;
 public class Part03Filtering {
 
     /**
-     * TODO 03.01
+     * 03.01
      * <p>
      * Return only the even numbers emitted by the given publisher
      */
     Flux<Integer> filterEven(Flux<Integer> flux) {
-        return null;
+        return flux.filter(i -> i % 2 == 0);
     }
 
     /**
-     * TODO 03.02
+     * 03.02
      * <p>
      * Ignore the duplicates emitted by the given publisher
      */
     Flux<Integer> ignoreDuplicates(Flux<Integer> flux) {
-        return null;
+        return flux.distinct();
     }
 
     /**
-     * TODO 03.03
+     * 03.03
      * <p>
      * Emit the last element or 100 if the flux is empty.
      */
     Mono<Integer> emitLast(Flux<Integer> flux) {
-        return null;
+        return flux.last(100);
     }
 
     /**
-     * TODO 03.04
+     * 03.04
      * <p>
      * Ignore items until a value greater than 10 is emitted.
      */
     Flux<Integer> ignoreUntil(Flux<Integer> flux) {
-        return null;
+        return flux.skipUntil(i -> i > 10);
     }
 
     /**
-     * TODO 03.05
+     * 03.05
      * <p>
      * Expect at most one or empty and signal error if more.
      */
     Mono<Integer> expectAtMostOneOrEmpty(Flux<Integer> flux) {
-        return null;
+        return flux.singleOrEmpty();        
     }
-
 
     /**
-     * TODO 03.06
+     * 03.06
      *
-     * Filter the provided Publisher where the criteria is asynchronously computed via {@link #asyncFilter} method
+     * Filter the provided Publisher where the criteria is asynchronously computed
+     * via {@link #asyncFilter} method
      */
     Flux<Integer> asyncComputedFilter(Flux<Integer> flux) {
-        return null;
+        return flux.filterWhen(i -> asyncFilter(i));        
     }
-
-
 
     Mono<Boolean> asyncFilter(Integer integer) {
         return Mono.just(integer % 2 == 0).delayElement(Duration.ofMillis(500));
     }
-
-
 
 }
