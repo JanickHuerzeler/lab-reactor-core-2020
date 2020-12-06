8767d3be8e24abaaae21fe23c64cd19ef2a4e259
Janick <janick.huerzeler@students.fhnw.ch>
2020-12-06 19:54:11 +0100

    Part 02

--- src/main/java/ch/fhnw/reactor/Part02Transform.java
+++ src/main/java/ch/fhnw/reactor/Part02Transform.java
@@ -10,49 +10,53 @@ import reactor.util.function.Tuple2;
 public class Part02Transform {
 
     /**
-     * TODO 02.01
+     * 02.01
      * <p>
-     * Create a sequence which transforms the values in the given flux to their length.
+     * Create a sequence which transforms the values in the given flux to their
+     * length.
      */
     Flux<Integer> transformToLength(Flux<String> flux) {
-        return null;
+        return flux.map(str -> str.length());
     }
 
     /**
-     * TODO 02.02
+     * 02.02
      * <p>
-     * Create a sequence which transforms the provided string sequence into a stream of uppercase characters.
-     * Hint: Use method s.split("") to convert a string into a string array containing its characters.  
+     * Create a sequence which transforms the provided string sequence into a stream
+     * of uppercase characters. Hint: Use method s.split("") to convert a string
+     * into a string array containing its characters.
      */
     Flux<String> characters(Flux<String> flux) {
-        return null;
+        return flux.flatMap(str -> Flux.just(str.split("")));
     }
 
     /**
-     * TODO 02.03
+     * 02.03
      * <p>
-     * Merge the values emitted by flux1 and flux2 publishers into an interleaved merged sequence.
+     * Merge the values emitted by flux1 and flux2 publishers into an interleaved
+     * merged sequence.
      */
     Flux<String> combineInEmissionOrder(Flux<String> flux1, Flux<String> flux2) {
-        return null;
+        return Flux.merge(flux1, flux2);
     }
 
     /**
-     * TODO 02.04
+     * 02.04
      * <p>
      * Pair the values emitted by the flux1 publisher with the flux2 publisher.
      */
     public Flux<Tuple2<String, Integer>> pairValues(Flux<String> flux1, Flux<Integer> flux2) {
-        return null;
+        return Flux.zip(flux1, flux2);
     }
 
     /**
-     * TODO 02.05
+     * 02.05
      * <p>
-     * When the phoneNumber and deliveryAddress is available pair them into an Order Mono.
+     * When the phoneNumber and deliveryAddress is available pair them into an Order
+     * Mono.
      */
     public Mono<Order> combineValues(Mono<String> phoneNumber, Mono<String> deliveryAddress) {
-        return null;
+        return Mono.zip(phoneNumber, deliveryAddress).map(tuple -> new Order(tuple.getT1(), tuple.getT2()));
     }
 
     @Data
